c1cabcf657de87c174446f2c51a508d90c5955b1
sahilshahpatel <sahilshahpatel@gmail.com>
2018-03-03 14:16:55 -0500

    testing complete

--- testSearch.php
+++ testSearch.php
@@ -2,13 +2,15 @@
 <html>
 
 <body>
+	Hey!
 	<?php
-	$json = "food2fork.com/api/search?key=29c623e7eb9072ca0d13d2353c2e1a3f&q=".$_POST['query'];
+	$json = file_get_contents("https://food2fork.com/api/search?key=29c623e7eb9072ca0d13d2353c2e1a3f&q=".$_POST['query']);
 	$searchData = json_decode($json, true);
+	var_dump($searchData['recipes'][$_POST['number']]['recipe_id']);
 	
-	$firstRecipeJSON = "food2fork.com/api/get?key=29c623e7eb9072ca0d13d2353c2e1a3f&rId=".$searchData[$_POST['number']]['recipe_id'];
+	$firstRecipeJSON = file_get_contents("https://food2fork.com/api/get?key=29c623e7eb9072ca0d13d2353c2e1a3f&rId=".$searchData['recipes'][$_POST['number']]['recipe_id']);
 	$firstRecipe = json_decode($firstRecipeJSON, true);
-	echo $firstRecipe->"ingredients";
+	var_dump($firstRecipe['recipe']['ingredients']);
 	?>
 </body>
 
