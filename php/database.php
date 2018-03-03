d2843d38283077536dc09db89d5a213ec2f78692
sahilshahpatel <sahilshahpatel@gmail.com>
2018-03-03 15:06:06 -0500

    Testing PHP Constants

--- php/database.php
+++ php/database.php
@@ -1,11 +1,15 @@
 <?php
-
+    //Define constants
+    define("API_KEY", "29c623e7eb9072ca0d13d2353c2e1a3f");
+    define("API_BASE_URL", "https://food2fork.com/api/");
+    define("API_SEARCH_URL", "https://food2fork.com/api/search?key=".API_KEY);
+    define("API_GET_URL", "https://food2fork.com/api/get?key=".API_KEY);
+    
     $host = "localhost";
     $user = "root";
-    $dbname = "nhs_data";
+    $dbname = "HackRidge2018";
 
     $dsn = "mysql:host=".$host."; dbname=".$dbname;
 
     $pdo = new PDO($dsn,$user);
-
 ?>
---
+++ php/testSearch.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+	Hey!
+	<?php
+	include 'database.php';
+	
+	if(defined(API_KEY)){
+		echo 'defined';
+	}
+	else{
+		echo 'not defined';
+	}
+	$json = file_get_contents(API_SEARCH_URL."&q=".$_POST['query']);
+	$searchData = json_decode($json, true);
+	var_dump($searchData['recipes'][$_POST['number']]['recipe_id']);
+	
+	$firstRecipeJSON = file_get_contents(API_GET_URL."&rId=".$searchData['recipes'][$_POST['number']]['recipe_id']);
+	$firstRecipe = json_decode($firstRecipeJSON, true);
+	var_dump($firstRecipe['recipe']['ingredients']);
+	?>
+</body>
+
+</html>
