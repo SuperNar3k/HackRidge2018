c9835e63c4d6fda1a95109f657f9dc43a02000cf
Benwagrez <benwagrez@gmail.com>
2018-03-03 13:55:00 -0600

    pushing cursor fix

--- php/header.php
+++ php/header.php
@@ -4,7 +4,7 @@
 
 	<img id = "LPLogo" src = "https://www.lphs.org/cms/lib/IL01904769/Centricity/Template/GlobalAssets/images///logos/_default.png"><!--LPHS Logo-->
         
-	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:48px;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "LPNHS - Home">Lake Park High School National Honor Society</span></h1>
+	<h1 class = "baseText" style = "padding-bottom: 0px; margin-bottom:0px; color: #005da3;  font-size:48px;cursor: pointer;;"><span id = "LPNHS" onclick = "location.href='index.php'" title = "LPNHS - Home">Lake Park High School National Honor Society</span></h1>
 	<h2 class = "baseText" style = "padding-top: 0px; margin-top:0px; color: #666; font-size:18px;">Scholarship | Leadership | Character | Service</h2>
 	<?php if(isset($_SESSION["StudentID"])) : ?><!--Checking if user is logged in for either sign in or sign out button-->
 		<div id = "headerLogout" class = "headerSignIn"><button id = "headerLogoutButton">Sign Out</button></div>
