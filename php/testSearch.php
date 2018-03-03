0209f1194723174929b6a29cc73c0bc3e9569903
sahilshahpatel <sahilshahpatel@gmail.com>
2018-03-03 15:16:22 -0500

    TestSearch completed

--- php/testSearch.php
+++ php/testSearch.php
@@ -4,14 +4,8 @@
 <body>
 	Hey!
 	<?php
-	include 'database.php';
+	require 'database.php';
 	
-	if(defined(API_KEY)){
-		echo 'defined';
-	}
-	else{
-		echo 'not defined';
-	}
 	$json = file_get_contents(API_SEARCH_URL."&q=".$_POST['query']);
 	$searchData = json_decode($json, true);
 	var_dump($searchData['recipes'][$_POST['number']]['recipe_id']);
