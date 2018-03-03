101818a05094ddeddc00e77ba6c8218cb67025cd
sahilshahpatel <sahilshahpatel@gmail.com>
2018-03-03 17:17:44 -0500

    header changes

--- php/header.php
+++ php/header.php
@@ -1,16 +1,15 @@
 <!--This is PHP to be implemented in all pages-->
+<link rel="stylesheet" href="../css/header.css">
 <div id = "navBarWrapper">
     <nav id = "navBar" class = "topnav">
-        <?php if(isset($_SESSION["StudentID"])) : ?><!--Checking if student is logged in for different nav bar-->
-            <a id = "homeLink" href = "index.php"><img id = "logo" src = "..\rsc\text_logo.png"></a>
-            <a class = "baseText" id = "" href = "events.php">Community Events</a>
-            <a class = "baseText" id = "membersLink" href = "members.php">Members</a>
-			<a class = "baseText" id = "myProfileLink" href = "my-profile.php">My Profile</a>
-        <?php else :?><!--If not logged in then the nav bar below-->
-            <a class = "baseText" id = "homeLink" href = "index.php">Home</a>
-            <a class = "baseText" id = "communityInvolvementLink" href = "community-involvement.php">Community Events</a>
-            <a class = "baseText" id = "membersLink" href = "members.php">Members</a>
-			<a class = "baseText" id = "whatItTakesLink" href = "what-it-takes.php">What It Takes</a>
-        <?php endif; ?>
+    	<a id = "homeLink" href = "index.php"><img id = "logo" src = "../rsc/text_logo.png"></a>
+			<form id = "searchBarForm" method = "get" action = "search.php">	
+				<input type = "text" id = "searchBar" name = "query" placeholder = "Search for recipes by name">
+				<input type = "image" name = "submit" alt = "Search"> 
+			</form>
+			<?php if(isset($_SESSION["StudentID"])) : ?><!--Checking if student is logged in for different nav bar-->
+        <a class = "baseText" id = "suggestionsLink" href = "suggestions.php">What Should I Eat Today?</a>
+				<a class = "baseText" id = "myProfileLink" href = "my-profile.php">My Profile</a>
+			<?php endif; ?>
     </nav>
 </div>
