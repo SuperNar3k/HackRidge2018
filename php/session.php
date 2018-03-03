c632bee5c0768a86c4ceec6c87a7f374c9667dd6
Benwagrez <benwagrez@gmail.com>
2018-03-03 14:06:42 -0600

    Creating new files

---
+++ php/session.php
@@ -0,0 +1,49 @@
+<?php
+    session_start();
+    include "database.php";
+
+    // Checking previous input fields and assigning to variables
+
+        if(isset($_POST["email"])) {
+            $useremail = $_POST["email"];
+        } 
+        if(isset($_POST['password'])) {
+
+            $userpassword = $_POST['password'];
+        }
+   
+    // Pulling data from "students" where "Email" is the user inputed email
+
+        $sql = "SELECT * FROM students WHERE Email=:myEmail";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(["myEmail" => $useremail]); 
+        $user = $stmt->fetch(PDO::FETCH_OBJ);
+        $rowCount = $stmt->rowCount();
+
+    // Checking if there is a user with same email, if not, invalid
+
+        if ($rowCount != 1) { 
+
+            setcookie("LOGINERROR2","error", time() + (86400 * 30), "/");
+            header("location: login.php");
+            $dbEmail = $user->Email;
+            $dbpassHash =$user->P;
+
+        } else{
+            // If the user asswordHash;
+            $studentID = $user->StudentID;        
+
+            // Check if the password matches, if not, invalid
+
+                if($useremail === $dbEmail && password_verify($userpassword, $dbpassHash)) { 
+
+                    // If successfull, start SESSION with "StudentID"
+
+                        $_SESSION["StudentID"] = $studentID;
+                        header('Location: index.php'); 
+                } else{
+                    setcookie("LOGINERROR","error", time() + (86400 * 30), "/");
+                    header("location: login.php");
+                }
+        }
+?>
---
+++ php/signupConfirmed.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+//----------
+//username needs to come from session
+//----------
+    if(isset($_POST["firstNameSignUp"])) {
+        $firstname = $_POST["firstNameSignUp"];
+       
+    } 
+    else{setcookie("ERROR","First Name is blank.", time() + (86400 * 30), "/");header("location: error.php");}
+    if(isset($_POST["lastNameSignUp"])) {
+        
+        $lastname = $_POST["lastNameSignUp"];
+         
+    }
+    else{setcookie("ERROR","Last Name is blank.", time() + (86400 * 30), "/");header("location: error.php");}
+    if(isset($_POST["emailSignUp"])) {
+
+        $useremail = $_POST["emailSignUp"];
+ 
+    }
+    else{setcookie("ERROR","Email is blank.", time() + (86400 * 30), "/");header("location: error.php");}
+    if(isset($_POST["passwordSignUp"])) {
+        
+        $userpassword = $_POST["passwordSignUp"];
+         
+    }
+    else{setcookie("ERROR","Password is blank.", time() + (86400 * 30), "/");header("location: error.php");}
+    if(isset($_POST["passwordConfirmSignUp"])) {
+        
+        $userpasswordConfirm = $_POST["passwordConfirmSignUp"];
+         
+    }
+    else{setcookie("ERROR","Password confirm is blank.", time() + (86400 * 30), "/");header("location: error.php");}
+            
+   
+    
+
+    include "database.php";
+
+    //collecting rows of emails with same email
+    $sql = "SELECT * FROM user WHERE email=:myEmail";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(["myEmail" => $useremail]); 
+    $email = $stmt->fetch(PDO::FETCH_OBJ);
+    $rowCountemail = $stmt->rowCount();
+
+
+    if ($rowCountemail == 1) { 
+        
+        setcookie("ERROR","That email is already taken.", time() + (86400 * 30), "/");
+        header("location: error.php");
+
+    }else if ($userpassword != $userpasswordConfirm) { 
+        
+        setcookie("ERROR","Your password does not match the other.", time() + (86400 * 30), "/");
+        header("location: error.php");
+        
+    }else{ 
+        //if only data is unique
+        //create new user
+        $sql = "INSERT INTO `users`(`firstName`, `lastName`, `email`, `password`) VALUES (:name, :lastName,:email, :pass)";
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute(["name" => $firstname, "email" => $useremail, "lastName" => $lastname, "pass" => $userpassword]);
+        header("location: login.php");
+    }
+    ?>
