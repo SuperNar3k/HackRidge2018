caf2b802f15c2c86d648abe174efa4732840087d
Trigger1620 <jozeftokarczyk1620@gmail.com>
2018-03-03 21:17:48 -0600

    aeg

--- php/footer.php
+++ php/footer.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="../css/baseCSS.css">
 <div id="footer">
 	<a id="bottomtext" href="aboutus.php">About us</a>
-	<a id="bottomtext" href="">News</a>
-	<a id="bottomtext" href="">Feedback</a>
-	<a id="bottomtext" href="">FAQ</a>
-	<a id="bottomtext" href="">Privacy Policy</a>
-	<a id="bottomtext" href="">Terms of Use</a>
-	<a id="bottomtext" href="">Help</a>
-	<a id="bottomtext" href="">Contact Us</a>
+	<a id="bottomtext" href="news.php">News</a>
+	<a id="bottomtext" href="feedback.php">Feedback</a>
+	<a id="bottomtext" href="faq.php">FAQ</a>
+	<a id="bottomtext" href="privacypolicy.php">Privacy Policy</a>
+	<a id="bottomtext" href="termsofuse.php">Terms of Use</a>
+	<a id="bottomtext" href="help.php">Help</a>
+	<a id="bottomtext" href="contactus.php">Contact Us</a>
 </div>
---
+++ php/termsofuse.php
@@ -0,0 +1,34 @@
+<?php
+session_start(); 
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+
+        <title>Foodle - Terms of Use</title>
+        <link rel="shortcut icon" href="../rsc//favicon.ico" type="image/x-icon">
+        <link rel="stylesheet" href="../css/baseCSS.css">
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <script src="../js/headerJQuery.js"></script>
+        <script>
+            $(document).ready(function(){
+                $("#homeLink").addClass("active");
+            });
+        </script>
+
+    </head>
+
+    <header id = "header"><?php include "header.php"; ?></header>
+
+    <body>
+        <div id = "footerPusher">
+
+            <!--Home Page Main Image Card-->
+
+        </div>
+    </body>
+
+    <footer id = "footer"><?php include "footer.php"; ?></footer>
+
+</html>
