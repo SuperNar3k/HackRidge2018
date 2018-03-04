98f139f3454010d9258866c0eaf6283269a892da
Trigger1620 <jozeftokarczyk1620@gmail.com>
2018-03-03 21:08:51 -0600

    aefw

--- php/footer.php
+++ php/footer.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="../css/baseCSS.css">
 <div id="footer">
-	<a id="bottomtext">About us</a>
-	<a id="bottomtext">News</a>
-	<a id="bottomtext">Feedback</a>
-	<a id="bottomtext">FAQ</a>
-	<a id="bottomtext">Privacy Policy</a>
-	<a id="bottomtext">Terms of Use</a>
-	<a id="bottomtext">Help</a>
-	<a id="bottomtext">Contact Us</a>
+	<a id="bottomtext" href="aboutus.php">About us</a>
+	<a id="bottomtext" href="">News</a>
+	<a id="bottomtext" href="">Feedback</a>
+	<a id="bottomtext" href="">FAQ</a>
+	<a id="bottomtext" href="">Privacy Policy</a>
+	<a id="bottomtext" href="">Terms of Use</a>
+	<a id="bottomtext" href="">Help</a>
+	<a id="bottomtext" href="">Contact Us</a>
 </div>
