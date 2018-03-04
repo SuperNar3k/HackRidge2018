7c6a71a8929bc6d6c702af8ef8bd4e6bbb8deb67
Trigger1620 <jozeftokarczyk1620@gmail.com>
2018-03-04 08:47:16 -0600

    fixed recurrent ids (changed to classes)

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,8 @@
 <link rel="stylesheet" href="../css/baseCSS.css">
+
 <div id="footer">
-	<a id="bottomtext" href="aboutus.php">About us</a>
-	<a id="bottomtext" href="privacypolicy.php">Privacy Policy</a>
-	<a id="bottomtext" href="termsofuse.php">Terms of Use</a>
-	<a id="bottomtext" href="contactus.php">Contact Us</a>
+	<a class="bottomtext" href="aboutus.php">About us</a>
+	<a class="bottomtext" href="privacypolicy.php">Privacy Policy</a>
+	<a class="bottomtext" href="termsofuse.php">Terms of Use</a>
+	<a class="bottomtext" href="contactus.php">Contact Us</a>
 </div>
