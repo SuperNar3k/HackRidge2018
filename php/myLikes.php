d43ffd77c037e6ba752bb431e0d01c98dd20eb99
Trigger1620 <jozeftokarczyk1620@gmail.com>
2018-03-04 08:23:59 -0600

    Added slideToggling to myLikes array

--- php/myLikes.php
+++ php/myLikes.php
@@ -4,6 +4,16 @@
   require 'loginCheck.php';
   echo '<link rel="stylesheet" href="../css/recipeList.css">';
   
+  echo '<script>
+      $(document).ready(function(){
+        $(".slideUpInit").slideUp(0);
+      });
+
+      function toggle(index){
+        $("#slideToggledWrapper"+index).slideToggle();
+      }
+  </script>';
+
   $sql = "SELECT * FROM userrecipelikes WHERE userID= :userID";
   $stmt = $pdo->prepare($sql);
   $stmt-> execute(['userID' => $_SESSION['userID']]);
@@ -15,7 +25,8 @@
     $recipeJSON = file_get_contents(API_GET_URL."&rId=".$recipeLikes[$i]);
     $recipeData = json_decode($recipeJSON, true);
 
-    echo '<h3>', $recipeData['recipe']['title'], '</h3>';
+    echo '<h3 style = "cursor: pointer;" onclick = "toggle(', $i,')">', $recipeData['recipe']['title'], '</h3>';
+    echo '<div id="slideToggledWrapper', $i,'" class = "slideUpInit">';
     echo '<img src = "', $recipeData['recipe']['image_url'],'" alt = "Image: ', $recipeData['recipe']['title'],'" class  = "foodImg">';
     echo '<br><ul>';
     for($j = 0; $j<count($recipeData['recipe']['ingredients']); $j++){
@@ -23,6 +34,6 @@
     }
     echo '</ul>';
     echo '<a href = "', $recipeData['recipe']['source_url'],'" target = "_blank">Read More</a>';
-    echo '</div>';
+    echo '</div></div>';
   }
 ?>
