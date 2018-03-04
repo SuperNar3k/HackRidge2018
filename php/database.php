af9124f63b1815f7670e9e75ddc1de490c1871c5
Benwagrez <benwagrez@gmail.com>
2018-03-04 08:03:06 -0600

    updating API key

--- php/database.php
+++ php/database.php
@@ -1,6 +1,6 @@
 <?php
     //Define constants
-    define("API_KEY", "29c623e7eb9072ca0d13d2353c2e1a3f");
+    define("API_KEY", "4ab8b937d75843765322ce6db7d75659");
     define("API_BASE_URL", "https://food2fork.com/api/");
     define("API_SEARCH_URL", "https://food2fork.com/api/search?key=".API_KEY);
     define("API_GET_URL", "https://food2fork.com/api/get?key=".API_KEY);
--- php/userSearch.php
+++ php/userSearch.php
@@ -2,18 +2,20 @@
 include 'database.php';
 session_start(); 
 include 'logincheck.php';
-    $sql = "SELECT * FROM users WHERE userID=:UserID";
+    $sql = "SELECT * FROM users WHERE email=:email";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute(["UserID" => $_POST['userSearch']]); 
+    $stmt->execute(["email" => $_POST['userSearch']]); 
     $rowCount = $stmt->rowCount();
-    
-    if($rowCount==0){header('location: profile.php?userID="',$_POST['userRequesting'],'"');}
+    $url = 'location: profile.php?userID='.$_POST["userRequesting"];
+
+    if($rowCount==0){header($url);}
     else{
         $user = $stmt->fetch(PDO::FETCH_OBJ);
         $userID = $user->userID;
         $sql = "INSERT INTO `usertouserfriendspending`(`requesterID`, `recipientID`) VALUES (:userID0,:userID1)";
         $stmt = $pdo->prepare($sql);
         $stmt->execute(["userID0" => $_POST['userRequesting'],"userID1" => $userID]);
-        header('location: profile.php?userID="',$_POST['userRequesting'],'"');
+        echo $_POST["userRequesting"];
+        header($url);
     }
 ?>
