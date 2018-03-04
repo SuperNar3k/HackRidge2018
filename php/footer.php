984f1fd0f42fe976b3b8eb243afed574586d51b3
SuperNar3k <ohanyanarek@gmail.com>
2018-03-03 20:20:45 -0600

    a

--- php/footer.php
+++ php/footer.php
@@ -1,2 +1,11 @@
-<link rel="stylesheet" href="../css/footer.css">
-<h4 url:""class = "baseText" style = "font-size: 24px">About Us</h4>
+<link rel="stylesheet" href="../css/baseCSS.css">
+<div id="footer">
+	<a id="bottomtext">About us</a>
+	<a id="bottomtext">News</a>
+	<a id="bottomtext">Feedback</a>
+	<a id="bottomtext">FAQ</a>
+	<a id="bottomtext">Privacy Policy</a>
+	<a id="bottomtext">Terms of Use</a>
+	<a id="bottomtext">Help</a>
+	<a id="bottomtext">Contact Us</a>
+</div>
