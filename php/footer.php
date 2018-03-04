14e8f9c025f4aedc5defef295e475a300576d740
Trigger1620 <jozeftokarczyk1620@gmail.com>
2018-03-04 02:51:31 -0600

    rd

--- php/footer.php
+++ php/footer.php
@@ -1,7 +1,6 @@
 <link rel="stylesheet" href="../css/baseCSS.css">
 <div id="footer">
 	<a id="bottomtext" href="aboutus.php">About us</a>
-	<a id="bottomtext" href="faq.php">FAQ</a>
 	<a id="bottomtext" href="privacypolicy.php">Privacy Policy</a>
 	<a id="bottomtext" href="termsofuse.php">Terms of Use</a>
 	<a id="bottomtext" href="contactus.php">Contact Us</a>
